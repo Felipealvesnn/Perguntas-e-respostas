181df0387fb4c31bebb9d17d1c7c2d2b4a2e27f1
Felipealvesnn <felipe.alvesnn@gmail.com>
2021-09-10 22:59:59 -0300

    add o index
    add

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const app = express();
 
-app.get("/", (req,res)=>{
-    res.send("Bem vindo ao meu site!");
+app.set('view engine', 'ejs');
+
+app.get("/:nome?/:lang?", (req,res)=>{
+    var nome = req.params.nome;
+    var lang = req.params.lang;
+    res.render("index",{nome:nome,lang:lang, empresa:"empresa nada"});
 })
 
 app.listen(8080,()=>{
