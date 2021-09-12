af891be1e3f0a1b8c1ac34dce4d7a3221ce52001
Felipealvesnn <felipe.alvesnn@gmail.com>
2021-09-12 13:10:51 -0300

    add
    add o bootstrap

--- index.js
+++ index.js
@@ -2,12 +2,18 @@ const express = require("express");
 const app = express();
 
 app.set('view engine', 'ejs');
+app.use(express.static('public'));
 
-app.get("/:nome?/:lang?", (req,res)=>{
-    var nome = req.params.nome;
-    var lang = req.params.lang;
-    res.render("index",{nome:nome,lang:lang, empresa:"empresa nada"});
-})
+app.get("/", (req,res)=>{
+    //var nome= req.params.nome;
+
+    res.render("index");
+});
+app.get("/perguntar", (req,res)=>{
+
+    res.render("perguntar");
+});
+
 
 app.listen(8080,()=>{
     console.log("app rodandono");
