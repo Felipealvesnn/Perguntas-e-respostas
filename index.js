af9b9abf4705a041b3eb66a2c67e69f89b6b14e7
Felipealvesnn <felipe.alvesnn@gmail.com>
2021-09-14 22:19:53 -0300

    formatado
    formatado

--- index.js
+++ index.js
@@ -4,17 +4,17 @@ const app = express();
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
-app.get("/", (req,res)=>{
+app.get("/", (req, res) => {
     //var nome= req.params.nome;
 
     res.render("index");
 });
-app.get("/perguntar", (req,res)=>{
+app.get("/perguntar", (req, res) => {
 
     res.render("perguntar");
 });
 
 
-app.listen(8080,()=>{
+app.listen(8080, () => {
     console.log("app rodandono");
 });
