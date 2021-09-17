4481c5ce5e83dc16598b38a614b6ec2363b22594
Felipealvesnn <felipe.alvesnn@gmail.com>
2021-09-17 12:26:01 -0300

    att
    att

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const app = express();
 
-app.set('view engine', 'ejs');
+app.set('view engine', 'ejs'); //estou dizendo para o express usar o EJS como view engine
 app.use(express.static('public'));
+//bodyparser
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
+//rotas
 app.get("/", (req, res) => {
     //var nome= req.params.nome;
 
@@ -14,8 +18,10 @@ app.get("/view/perguntar", (req, res) => {
     res.render("perguntar");
 });
 
-app.post("/SalvarPerguntar", (req, res)=>{
-    res.send("formulario recebido!! ");
+app.post("/SalvarPerguntar", (req, res) => {
+    var titulo= req.body.titulo;
+    var descricao= req.body.descricao;
+    res.send("formulario recebido!! "+ titulo +"  "+ "descricao"+ descricao);
 
 });
 
@@ -23,3 +29,7 @@ app.post("/SalvarPerguntar", (req, res)=>{
 app.listen(8080, () => {
     console.log("app rodandono");
 });
+
+function clicar(){
+    window.alert("tu cliocu");
+}
